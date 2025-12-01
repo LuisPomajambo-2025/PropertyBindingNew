7066ef43ab6d7db0bbd2f8bad7f78182e14322d2
Usuario <jambostackboot@gmail.com>
2025-12-01 19:17:53 +0100

    formulario

--- src/app/ejercicios-ROUTING/ejercicio0/formulario-de-contacto/formulario-de-contacto.ts
+++ src/app/ejercicios-ROUTING/ejercicio0/formulario-de-contacto/formulario-de-contacto.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // 👈 necesario para ngModel
 
 @Component({
   selector: 'app-formulario-de-contacto',
-  imports: [],
+  standalone: true, // 👈 obligatorio para standalone component
+  imports: [FormsModule], // 👈 importa FormsModule aquí
   templateUrl: './formulario-de-contacto.html',
   styleUrl: './formulario-de-contacto.css',
 })
 export class FormularioDeContacto {
+  // Modelo del formulario
+  contacto = {
+    nombre: '',
+    apellidos: '',
+    email: '',
+    telefono: ''
+  };
+
+  enviar() {
+    console.log('Formulario enviado:',this.contacto);
+    alert('Formulario enviado correctamente: '+ this.contacto.nombre +"/"+this.contacto.apellidos+"/"+this.contacto.email+"/"+this.contacto.telefono);
+  }
 
 }
